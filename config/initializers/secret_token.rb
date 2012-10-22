da4400d2661451d80445aaedf6abff435143e9a1
Mike Chen <chagel@gmail.com>
2012-10-22 21:58:17 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Akita::Application.config.secret_token = '5b4adef1971b8ec5e112e6c9c819b1835cd355a2a54196d5d214ed25526464b924a7819476b94df24a46fda7ff6d463c4b6389411ff2081a12a1e56f5952dcfd'

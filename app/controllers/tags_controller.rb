3d114935e26253fa8b88e823d041420f3c903f01
Mike Chen <chagel@gmail.com>
2013-05-25 23:26:19 +0800

    fix tag items order

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -3,7 +3,7 @@ class TagsController < ApplicationController
 
 	def show
 		@tag = Tag.find_by(slug: params[:slug])
-		@links = Link.where("tags.id" => @tag.id).page(params[:page])
+		@links = Link.where("tags.id" => @tag.id).desc(:created_at).page(params[:page])
 	end
 
 	def index
